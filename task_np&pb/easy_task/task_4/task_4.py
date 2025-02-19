3a702972dcaaf5b61a8ae08f543f4fc20864901b
Egor <142446696+fire0clop@users.noreply.github.com>
2025-02-18 23:24:22 -0300

    add Task 4

---
+++ task_np&pb/easy_task/task_4/task_4.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+value = np.random.randint(1, 11, 12)
+month = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+       'August', 'September', 'October', 'November', 'December']
+data = pd.DataFrame({'month': month, 'revenue': value})
+print('Average is',data['revenue'].mean().round(2))
+print('Minimum is',data['revenue'].min())
+print('Maximum is',data['revenue'].max())
+indx = 0
+list_dif = []
+while indx < len(data['month']):
+    if indx == 0:
+        list_dif.append(0)
+        indx += 1
+    else:
+        list_dif.append(data.at[indx, 'revenue'] - data.at[indx - 1, 'revenue'])
+        indx += 1
+data['Grow'] = list_dif
+print(data)
+
+#Когда искал альтернативы решению узнал о методе diff(),
+# то есть можно без цикла и ручной индексации вычислить разницу текущего и предыдущего значения
